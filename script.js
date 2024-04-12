d602bbf37c1627454039f01a2fa43946504b855f
anuragv668 <anuragvarma668@gmail.com>
2024-04-12 21:16:02 +0530

    refactor

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-function Book (title, author, pages = 0, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  if (read == true) {
-    this.read = 'read';
-  } else {
-    this.read = "not read";
+class Book {
+  constructor(title, author, pages = 0, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    if (read == true) {
+      this.read = 'read';
+    } else {
+      this.read = "not read";
+    }
   }
-  this.info = () => {
+  info() {
     return `The ${this.title} by ${this.author}, ${this.pages} pages. `;
   }
 }
+
+
+// function Book (title, author, pages = 0, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   if (read == true) {
+//     this.read = 'read';
+//   } else {
+//     this.read = "not read";
+//   }
+//   this.info = () => {
+//     return `The ${this.title} by ${this.author}, ${this.pages} pages. `;
+//   }
+// }
 
 const library = [];
 
